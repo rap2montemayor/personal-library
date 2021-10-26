f0e82095d7694df8f36c1c54140f8f6785137d9d
rap2montemayor <rap2montemayor@gmail.com>
2021-10-26 22:29:24 +0800

    Added notes to binary and ternary search

--- Searching/Binary Search (int).cpp
+++ Searching/Binary Search (int).cpp
@@ -1,12 +1,26 @@
-bool obj(int m) {
-    //
+// Binary Search on Integers
+//
+// Returns the last integer i such that obj(i) < x.
+//
+// Note:
+// - This implementation assumes obj monotonically increases
+// - Behavior can be finnicky when changes are made on
+//      - the comparison
+//      - how l and r are updated
+//      - how m is computed
+// - Make sure to test when making changes
+// - Consider using lower_bound() or upper_bound() if you are simply searching
+//      for an element in an array.
+
+int obj(int m) {
+    return a[m];
 }
 
 int find(int x) {
     int l = 0, r = n, m;
     while (r-l > 1) {
         m = (l+r) >> 1;
-        if (obj(m)) l = m;
+        if (obj(m) < x) l = m;
         else r = m;
     }
     return l;
--- Searching/Ternary Search (int).cpp
+++ Searching/Ternary Search (int).cpp
@@ -1,14 +1,27 @@
+// Ternary Search on Integers
+//
+// Returns an integer i such that obj(i) is maximal.
+//
+// Note:
+// - This implementation assumes that obj is bitonic, and opens downards
+// - Don't forget to check for literal edge cases.
+// - Behavior can be finnicky like binary search. Make sure to test.
+// - This implementation can be thought of as binary searching for 0 on the
+//      differences between adjacent elements. This is analogous to getting the
+//      zeroes of a derivative of a quadratic function.
+
 int obj(int m) {
-	// 
+    return a[m];
 }
 
-// Returns index of largest value
 int find() {
     int l = -1, r = n, m;
     while (r-l > 1) {
         m = (l+r) >> 1;
-        if (obj(m) < obj(m+1)) l = m;
-        else r = m;
+        if (obj(a, m) < obj(a, m+1))
+            l = m;
+        else
+            r = m;
     }
     return l+1;
 }
