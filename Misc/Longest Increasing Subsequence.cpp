663842b35d7c27a2eff644444be61b9228c8e24e
rap2montemayor <rap2montemayor@gmail.com>
2021-10-26 22:32:05 +0800

    Added notes to longest increasing subsequence

--- Misc/Longest Increasing Subsequence.cpp
+++ Misc/Longest Increasing Subsequence.cpp
@@ -1,3 +1,23 @@
+// Longest Increasing Subsequence
+//
+// Gets the longest increasing subsequence of an array, a. The LIS is given as
+// an array of indices.
+//
+// Notes:
+// - This is done with a combination of DP and binary search.
+// - dp[i] contains the index of the last element that an increasing
+//      subsequence of length i+1 would contain.
+// - dp[0] is initialized as a[0], while the rest of dp is initialized as INF.
+// - Values in dp will always be increasing as it is updated with values from
+//      a. Hence, the position of the next value of a can be found using
+//      binary search.
+// - Binary search is handled by upper_bound(), which gives the last position
+//      that an element can be inserted into without breaking the ordering.
+// - par[i] contains a pointer to the previous element before a[i] if it were
+//      included in an increasing subsequence.
+// - If only the length of an LIS is needed, and not an actual LIS, len may be
+//      returned instead of constructlis()
+
 int dp[MAXN] = {}, par[MAXN] = {};
 vi constructlis(int idx) {
     vi lis({idx});
