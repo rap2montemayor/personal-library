7dfc6daf49bbcd8a2bae387a0c32ee973d497ab4
rap2montemayor <rap2montemayor@gmail.com>
2021-11-07 01:30:50 +0800

    Updated ordered statistics tree

--- Data Structures/Ordered Statistics Tree.cpp
+++ Data Structures/Ordered Statistics Tree.cpp
@@ -2,8 +2,11 @@
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template <typename T>
-using index_set = tree<T, null_type, std::less<T>,
-splay_tree_tag, tree_order_statistics_node_update>;
-// indexed_set<int> t; t.insert(...);
-// t.find_by_order(index); // 0-based
+using ordered_set = __gnu_pbds::tree<T, null_type, less<T>,
+	rb_tree_tag, tree_order_statistics_node_update>;
+// Usage:
+// ordered_set<int> t;
+// t.insert(key)
+// t.find_by_order(index); // 0-based. returns iterator to key
 // t.order_of_key(key);
+// t.erase(key);
