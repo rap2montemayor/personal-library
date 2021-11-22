7a1b3a2aaa22195675971f1d9f81a8a54a6b4c5c
rap2montemayor <rap2montemayor@gmail.com>
2021-11-22 20:40:02 +0800

    Updated annotations on LIS

--- Misc/Longest Increasing Subsequence.cpp
+++ Misc/Longest Increasing Subsequence.cpp
@@ -1,24 +1,18 @@
 // Longest Increasing Subsequence
 //
-// Gets the longest increasing subsequence of an array, a. The LIS is given as
-// an array of indices.
+// Longest increasing subsequence of an array in O(n logn).
+// dp[i] contains the index of the last element that an increasing
+//      subsequence of length i+1 would contain. For each element in the array,
+//      its position in dp is found using binary search.
 //
 // Notes:
-// - This is done with a combination of DP and binary search.
-// - dp[i] contains the index of the last element that an increasing
-//      subsequence of length i+1 would contain.
-// - dp[0] is initialized as a[0], while the rest of dp is initialized as INF.
-// - Values in dp will always be increasing as it is updated with values from
-//      a. Hence, the position of the next value of a can be found using
-//      binary search.
-// - Binary search is handled by upper_bound(), which gives the last position
-//      that an element can be inserted into without breaking the ordering.
-// - par[i] contains a pointer to the previous element before a[i] if it were
-//      included in an increasing subsequence.
-// - If only the length of an LIS is needed, and not an actual LIS, len may be
-//      returned instead of constructlis()
+// - LIS is given as an array of indices
+// - Combines DP and binary search.
+// - dp[0] is initialized as 0, while the rest of dp is initialized as INF.
+// - Values in dp will always be increasing as it is updated with new values.
+//      Hence, binary search can be used to find the position of new values.
 
-int dp[MAXN] = {}, par[MAXN] = {};
+int dp[MAXN] = {}, par[MAXN];
 vi constructlis(int idx) {
     vi lis({idx});
     while (par[idx] != idx)
@@ -30,14 +24,13 @@ vi constructlis(int idx) {
 bool liscomp(int l, int r) { return r == INF or a[l] <= a[r]; }
 vi getlis() {
     fill(dp+1, dp+n, INF);
+    int len = 1;
     for (int i = 1; i < n; ++i) {
         int pos = upper_bound(dp, dp+n, i, liscomp) - dp;
         dp[pos] = i;
+        len = max(len, pos);
         if (pos > 0) par[i] = dp[pos-1];
         else par[i] = i;
     }
-    int len = 0;
-    for (int i = 0; i < n; ++i)
-        if (dp[i] != INF) len = dp[i];
     return constructlis(len);
 }
